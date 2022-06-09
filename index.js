471939f626788d2dd199ddc6c2727172cfd96647
Keegan McRoberts <keegan.mcroberts@gmail.com>
2022-06-09 17:39:46 -0500

    commit tree b431d6cf3549e51afb7ff558828f6ca9cdead4d4

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+    let hQ = 42;
+    return Math.abs(block - hQ);
+}
+distanceFromHqInBlocks(43); // return value 1 
+distanceFromHqInBlocks(50); // return value 8 
+distanceFromHqInBlocks(34); // return value 8
+
+function distanceFromHqInFeet(feet) {
+    distanceFromHqInBlocks(feet);
+        let distance = distanceFromHqInBlocks(feet);
+
+        return distance * 264;
+}
+distanceFromHqInFeet(43)
+distanceFromHqInFeet(50)
+distanceFromHqInFeet(34)
+
+function distanceTravelledInFeet(start, destination){
+    return Math.abs((start-destination)*264)
+}
+distanceTravelledInFeet(43, 48);
+distanceTravelledInFeet(50, 60);
+distanceTravelledInFeet(34, 28);
+
+function calculateFarePrice(start, destination){
+    
+    return 
+
+}
